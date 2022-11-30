48a0fcc547b5fa3c0f97491ec8089890960783ef
KseniyaPanda <ksenia-22-11@mail.ru>
2022-11-30 21:29:47 +0300

    решение 15 задачи

--- homework015/Program.cs
+++ homework015/Program.cs
@@ -6,3 +6,18 @@
 1 -> нет
 */
 
+Console.WriteLine("Введите номер дня недели");
+int number = int.Parse(Console.ReadLine());
+
+if (number > 0 && number < 6)
+{
+   Console.WriteLine("Рабочий день");
+}
+else if (number == 6 || number == 7)
+{
+   Console.WriteLine("Выходной день");
+}
+else
+{
+   Console.WriteLine("Введен не номер дня недели. Необходимо число от 1 до 7");
+}
