a2c301e3c72bd3ae58064ffc40d8a47fe99e8c9d
KseniyaPanda <ksenia-22-11@mail.ru>
2022-11-30 21:14:45 +0300

    решение 13 задачи

--- homework013/Program.cs
+++ homework013/Program.cs
@@ -6,30 +6,6 @@
 32679 -> 6
 */
 
-/*
-Console.WriteLine("Введите число");
-int number = int.Parse(Console.ReadLine());
-int result = number % 10; 
-
-if (number > 999)
-{
-   while (number > 999)
-   {
-      number = number / 10;
-   }
-   result = number % 10;
-   Console.WriteLine($"третье число {result}");
-}
-else if (number < 100)
-{
-   Console.WriteLine($"в числе {number} нет третьей цифры");
-}
-else
-{
-   Console.WriteLine($"третье число {result}");
-}
-
-*/
 Console.WriteLine("Введите число");
 int number = int.Parse(Console.ReadLine());
 
